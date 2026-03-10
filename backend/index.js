849accb88545741b73eed6916c9f3fda9875f12b
waqas-codes <muhammadwaqas96845@gmail.com>
2026-03-10 12:00:00 +0500

    Added commit

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,4 @@
-// const express = require("express");
+const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const http = require("http");
@@ -53,7 +53,7 @@ app.use(errorHandler);
 
 const server = http.createServer(app);
 
-const PORT = process.env.PORT || 5500;
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
